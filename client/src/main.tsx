41a49c5dfbf47de38e4b83675d57ab0a8e4bf044
Will Rutland <wrutland07@gmail.com>
2025-09-03 12:16:36 +0100

    scrapped header

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './scss/index.scss';
+import Footer from './components/multiPageComponents/footer';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Footer/>
   </StrictMode>,
 );
