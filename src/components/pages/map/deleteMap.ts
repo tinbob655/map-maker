ba9309b6c89e586e8f30bbb456239cc12e02eab7
Will Rutland <wrutland07@gmail.com>
2025-09-06 13:31:48 +0100

    added function to delete maps

---
+++ src/components/pages/map/deleteMap.ts
@@ -0,0 +1,24 @@
+import type MapClass from "./mapClass";
+import saveMaps from "./saveMaps";
+
+export default function deleteMap(mapName:string, userMaps:MapClass[]):MapClass[] {
+
+    //find the map to delete
+    const deleteIndex:number = userMaps.findIndex((map) => {
+        if (map.name === mapName) {
+            return true;
+        }
+        else return false;
+    });
+
+    //make sure the map to delete was found
+    if (deleteIndex === -1) {
+        throw new Error('Could not find map to delete');
+    };
+
+    userMaps.splice(deleteIndex, 1);
+    
+    //save and return the new maps array
+    saveMaps(userMaps);
+    return userMaps;
+};
